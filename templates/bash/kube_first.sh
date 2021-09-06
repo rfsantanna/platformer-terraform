fa047f9cb972e11ac47ca8eaa4a667c6b57067a2
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-06 17:06:54 -0300

    fiox

---
+++ templates/bash/kube_first.sh
@@ -0,0 +1,14 @@
+curl https://docs.projectcalico.org/manifests/calico.yaml -O
+
+
+cat > kubeadm-config.yaml <<EOF 
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+kubernetesVersion: 1.20.1
+controlPlaneEndpoint:"k8scp:6443"
+networking:
+  podSubnet: 192.168.0.0/16
+
+EOF
+
+kubeadm init --config=kubeadm-config.yaml --upload-certs | tee kubeadm-init.out
--- templates/bash/kube_ubuntu.sh
+++ templates/bash/kube_ubuntu.sh
@@ -1,12 +1,13 @@
-apt update -y
+sleep 10
+sudo apt update -y
 
-apt install docker.io -y
-echo 'deb  http://apt.kubernetes.io/  kubernetes-xenial  main' > /etc/apt/sources.list.d/kubernetes.list
+sudo apt install docker.io -y
+echo 'deb  http://apt.kubernetes.io/  kubernetes-xenial  main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-apt update -y
+sudo apt update -y
 
-apt-get install -y kubeadm=1.20.1-00 kubelet=1.20.1-00 kubectl=1.20.1-00
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y kubeadm=1.20.1-00 kubelet=1.20.1-00 kubectl=1.20.1-00
+sudo apt-mark hold kubelet kubeadm kubectl
 
