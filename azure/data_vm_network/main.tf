663ffea1cec5421ceb8cd6995b2fa5671cd1da7e
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-06 16:32:26 -0300

    fix

--- azure/data_vm_network/main.tf
+++ azure/data_vm_network/main.tf
@@ -30,7 +30,7 @@ data "azurerm_virtual_network" "vnet" {
 }
 
 data "azurerm_subnet" "subnet" {
-  name = "${var.vnet_name}-subnet"
+  name = var.subnet_name
   virtual_network_name  = data.azurerm_virtual_network.vnet.name
   resource_group_name = var.resource_group_name
 }
