a9d077d10f31ef2c6bc7f991f8cd90f6774a07a1
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-10 19:41:52 -0300

    Add vm output

--- azure/vm_linux_spot/outputs.tf
+++ azure/vm_linux_spot/outputs.tf
@@ -1,3 +1,12 @@
 output "public_ip" {
   value = data.azurerm_public_ip.updated
 }
+
+output "vm" {
+  value = {
+    name           = azurerm_linux_virtual_machine.vm.name
+    resource_group = azurerm_linux_virtual_machine.vm.resource_group_name
+    location       = azurerm_linux_virtual_machine.vm.location
+    image          = azurerm_linux_virtual_machine.vm.source_image_reference
+  }
+}
