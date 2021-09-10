797f1b4f9fd98cfda56cc08965590c579ab5b605
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-10 18:36:12 -0300

    fix variables

--- templates/bash/mount.sh
+++ templates/bash/mount.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 
 
-mkdir ${mount_path}
+mkdir ${path}
 if [ ! -d "/etc/smbcredentials" ]; then
  mkdir /etc/smbcredentials
 fi
-if [ ! -f "/etc/smbcredentials/${mount_user}.cred" ]; then
-    echo 'username=${mount_user}' > /etc/smbcredentials/${mount_user}.cred
-    echo 'password=${mount_pass}' >> /etc/smbcredentials/${mount_user}.cred
+if [ ! -f "/etc/smbcredentials/${user}.cred" ]; then
+    echo 'username=${user}' > /etc/smbcredentials/${user}.cred
+    echo 'password=${pass}' >> /etc/smbcredentials/${user}.cred
 fi
-chmod 600 /etc/smbcredentials/${mount_user}.cred
+chmod 600 /etc/smbcredentials/${user}.cred
 
-echo "${mount_share} ${mount_path} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${mount_user}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
-mount -t cifs ${mount_share} ${mount_path} -o vers=3.0,credentials=/etc/smbcredentials/${mount_user}.cred,dir_mode=0777,file_mode=0777,serverino
+echo "${share} ${path} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${user}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
+mount -t cifs ${share} ${path} -o vers=3.0,credentials=/etc/smbcredentials/${user}.cred,dir_mode=0777,file_mode=0777,serverino
 
