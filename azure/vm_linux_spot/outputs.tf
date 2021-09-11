5a4be3b5e6fcec072c0bb7d05cd970c2f59d8cb9
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-11 18:50:34 -0300

    add more output

--- azure/vm_linux_spot/outputs.tf
+++ azure/vm_linux_spot/outputs.tf
@@ -4,14 +4,22 @@ output "public_ip" {
 
 output "vm" {
   value = {
-    name           = azurerm_linux_virtual_machine.vm.name
-    resource_group = azurerm_linux_virtual_machine.vm.resource_group_name
-    location       = azurerm_linux_virtual_machine.vm.location
-    image          = azurerm_linux_virtual_machine.vm.source_image_reference[0]
+    id              = azurerm_linux_virtual_machine.vm.id
+    name            = azurerm_linux_virtual_machine.vm.name
+    size            = azurerm_linux_virtual_machine.vm.size
+    resource_group  = azurerm_linux_virtual_machine.vm.resource_group_name
+    location        = azurerm_linux_virtual_machine.vm.location
+    admin_username  = azurerm_linux_virtual_machine.vm.admin_username
+    priority        = azurerm_linux_virtual_machine.vm.priority
+    eviction_policy = azurerm_linux_virtual_machine.vm.eviction_policy
+    private_ip      = azurerm_linux_virtual_machine.vm.private_ip_address
+    public_ip       = azurerm_linux_virtual_machine.vm.public_ip_address
+    image           = azurerm_linux_virtual_machine.vm.source_image_reference[0]
+    os_disk         = azurerm_linux_virtual_machine.vm.os_disk[0]
   }
 }
 
 output "vm_full" {
-  value    = azurerm_linux_virtual_machine.vm
+  value     = azurerm_linux_virtual_machine.vm
   sensitive = true
 }
