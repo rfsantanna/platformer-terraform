01b70ddc23d945b73fe359616b3043f36de2d9e7
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-12 22:37:24 -0300

    include disk defaults

--- azure/vm_linux_spot/variables.tf
+++ azure/vm_linux_spot/variables.tf
@@ -8,8 +8,14 @@ variable "vnet_addr" { type = string }
 variable "vnet_name" { type = string }
 variable "admin_username" { type = string }
 variable "admin_publickey" { type = string }
-variable "disk_size" { type = string }
-variable "disk_type" { type = string }
+variable "disk_size" {
+  type = string
+  default = "60"
+}
+variable "disk_type" {
+  type = string
+  default = "Standard_LRS"
+}
 variable "dynamic_ip" {
   type    = bool
   default = true
