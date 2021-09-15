eb3db5306a718d5b3485795c550437f8b25269a0
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-15 12:06:47 -0300

    remove vnet_rg var

--- azure/data_vm_network/variables.tf
+++ azure/data_vm_network/variables.tf
@@ -3,7 +3,6 @@ variable "vnet_name" {type = string}
 variable "subnet_name" {type = string}
 variable "dynamic_ip" {type = bool}
 variable "vm_rg" {type = string}
-variable "vnet_rg" {type = string}
 variable "static_ip" {
   type = string 
   default = "static-ip"
