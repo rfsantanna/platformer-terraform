e894fa7c1be2aec3d0040f0c731044e17717684e
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-15 11:18:38 -0300

    remove vnet/subnet addrs

--- azure/data_vm_network/variables.tf
+++ azure/data_vm_network/variables.tf
@@ -1,8 +1,6 @@
 variable "id" {type = string}
 variable "vnet_name" {type = string}
-variable "vnet_addr" {type = string}
 variable "subnet_name" {type = string}
-variable "subnet_addr" {type = string}
 variable "dynamic_ip" {type = bool}
 variable "resource_group_name" {type = string}
 variable "static_ip" {
