8bbd1e7b0409e7ba709fc526ee5337141c57bce1
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-11-22 11:10:49 -0300

    Include naming module

---
+++ naming/main.tf
@@ -0,0 +1,16 @@
+locals {
+  platforms = {
+    for file in var.platform_files
+    : trimsuffix(basename(file), "*.yml") => yamldecode(file(file))
+  }
+  github_repos = {
+    for platform, config in local.platforms
+    : platform => config
+    if contains(keys(config), "github")
+  }
+  azure_credentials = {
+    for platform, config in local.platforms
+    : platform => config
+    if contains(keys(config.credentials), "azure_service_principal")
+  }
+}
---
+++ naming/outputs.tf
@@ -0,0 +1,11 @@
+output "platforms" {
+  value = local.platforms
+}
+
+output "github_repos" {
+  value = local.github_repos
+}
+
+output "azure_credentials" {
+  value = local.azure_credentials
+}
