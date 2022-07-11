8f35e59118589cdb3f99e493e839765a97b1101d
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2022-07-11 00:57:14 -0300

    add http port

--- azure/data_vm_network/main.tf
+++ azure/data_vm_network/main.tf
@@ -87,6 +87,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "http"
+    priority                   = 304
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
